195c7381927e861ce1d56dfb4bc4a20d48225067
angov-vangel <angov.vangel@gmail.com>
2023-05-13 16:08:59 +0200

    CreateEvent component added

--- platform/pages/index.tsx
+++ platform/pages/index.tsx
@@ -1,10 +1,11 @@
+import CreateEvent from "@/components/CreateEvent";
 import UserFormRegister from "@/components/UserFormRegister";
 import React from "react";
 export default function Home() {
   return (
     <div className="">
       <UserFormRegister />
-      <div>Proba</div>
+      <CreateEvent />
     </div>
   );
 }
